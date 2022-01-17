f17eb402887469bff181854399ea056522891980
Felix Obenhuber <felix@obenhuber.de>
2022-01-17 16:55:45 +0100

    Add convenience functions to header
    Add build and getters for the SomeIP header struct. Promote missing docs
    to warnings. Add some struct field annotations.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
-//! A Rust library for parsing the SOME/IP network protocol (without payload interpretation).
-//!
+//! Crate for parsing the SOME/IP network protocol (without payload interpretation).
+
+#![warn(missing_docs)]
+
 mod error;
+/// Parse someip messages
 pub mod parser;
+/// Serialize someip messages
 pub mod serializer;
 mod types;
 
